88856cad9392f78039cbaac6355fbe09eb33182c
blagebo <blagebo@proton.me>
2026-04-29 16:53:06 +0300

    after flake8 and pylint

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,29 @@
 #! /usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: blagebo
+Purpose: Say hello
+"""
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name',
-                    default='World', help='Name to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+
+# ---------------------------------------------
+def get_args():
+    """Get the command line arguments"""
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+# ---------------------------------------------
+def main():
+    """My main man"""
+
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
