b2054c308f04b47625d8c858967c03e7becff1e2
blagebo <blagebo@proton.me>
2026-05-05 17:13:02 +0300

    added positional argument 'word'

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -13,38 +13,12 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Crowsnest - choose the correct article',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('word',
                         metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        help='A word')
 
     return parser.parse_args()
 
@@ -54,8 +28,11 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
+    print(args)
+    word = args.word
+    print(word)
+
+    """int_arg = args.int
     file_arg = args.file
     flag_arg = args.on
     pos_arg = args.positional
@@ -64,7 +41,9 @@ def main():
     print(f'int_arg = "{int_arg}"')
     print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
     print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(f'positional = "{pos_arg}"')"""
+
+
 
 
 # --------------------------------------------------
